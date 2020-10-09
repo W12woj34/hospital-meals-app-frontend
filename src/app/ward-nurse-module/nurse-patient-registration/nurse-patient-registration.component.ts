d669e67445f019b540bf48037d71f89c9e4c28da
W12woj34 <woj120@gmail.com>
2020-10-10 01:30:22 +0200

    add nurse registration layout

---
+++ src/app/ward-nurse-module/nurse-patient-registration/nurse-patient-registration.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+import {NgForm} from '@angular/forms';
+import {ConfirmDialogComponent} from '../../tools-module/confirm-dialog/confirm-dialog.component';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {Router} from '@angular/router';
+import {ApiService} from '../../service/api.service';
+
+export interface PatientRegister {
+  pesel: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+}
+
+@Component({
+  selector: 'app-nurse-patient-registration',
+  templateUrl: './nurse-patient-registration.component.html',
+  styleUrls: ['./nurse-patient-registration.component.css']
+})
+
+
+export class NursePatientRegistrationComponent implements OnInit {
+
+  patient: PatientRegister = {pesel: '', firstName: '', lastName: '', birthDate: ''};
+  color: ThemePalette = 'primary';
+  firstTime = false;
+  dialogResult;
+
+  constructor(public dialogRef: MatDialogRef<NursePatientRegistrationComponent>,
+              public dialog: MatDialog,
+              private api: ApiService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(loginForm: NgForm): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      minWidth: 'fit-content',
+      data: {result: this.dialogResult}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.dialogResult = result;
+      console.log(this.dialogResult);
+      if (this.dialogResult === false) {
+        return;
+      }
+
+    /*  this.api.login('/login', this.patient).subscribe(
+        (r => {
+
+          }
+        ));
+
+     */
+// tu trzeba będzie zapisać wszystko
+      console.log('The dialog was closed');
+      this.dialogRef.close(true);
+    });
+
+
+  }
+}
