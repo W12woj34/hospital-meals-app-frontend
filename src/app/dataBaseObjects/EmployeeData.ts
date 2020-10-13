71abcfecd2079bad8ea7362bf6830ace701bb995
W12woj34 <woj120@gmail.com>
2020-10-13 14:01:21 +0200

    add additional dto

---
+++ src/app/dataBaseObjects/EmployeeData.ts
@@ -0,0 +1,9 @@
+export interface EmployeeData {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  pesel: number;
+  role: string;
+  ward: string;
+}
---
+++ src/app/dataBaseObjects/PatientMealOrder.ts
@@ -0,0 +1,11 @@
+export interface PatientMealOrder {
+  id: number;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  pesel: number;
+  ward: string;
+  breakfast: boolean;
+  lunch: boolean;
+  supper: boolean;
+}
